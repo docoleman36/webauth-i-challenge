71e445976ea19b5f234b12a9093d0bdc2e8a64ad
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2019-12-08 20:45:27 -0800

    Completed project

--- api/server.js
+++ api/server.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
 
 const authRouter = require('../auth/auth-router.js');
 const userRouter = require('../users/users-router');
 
 const server = express();
 
+const sessionConfig = {
+    name: "session",
+    secret: "some secret here",
+    cookie: {
+        maxAge: 1000 * 30,
+        secure: false, //true in production
+        httpOnly: true,
+    },
+    resave: false,
+    saveUninitialized: false,
+};
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
-    res.json({ api: "IT's alive "})
+    res.json({ api: "IT's alive " })
 })
 
 
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 
 const Users = require('../users/users-model');
-// const restricted = require('./restricted-middleware');
+const restricted = require('./restricted-middleware');
 
 router.post('/register', (req, res) => {
     let user = req.body;
@@ -24,8 +24,9 @@ router.post('/login', (req, res) => {
 
     Users.findBy({ username })
         .first()
-        .then(users => {
-            if (user) {
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                req.session.user = user;
                 res.status(200).json({ message: `Welcome ${user.username}!` });
             } else {
                 res.status(401).json({ message: 'Invalid Credentials' });
@@ -34,6 +35,20 @@ router.post('/login', (req, res) => {
         .catch(error => {
             res.status(500).json(error);
         });
+})
+
+router.get('/logout', restricted, (req, res) => {
+    if (req.session) {
+        req.session.destroy(err => {
+            if (err) {
+                res.json({ message: `couldn't log you out` })
+            } else {
+                res.status(204).end();
+            }
+        })
+    } else {
+        res.status(200).json({ message: 'you were never here' })
+    }
 })
 
 module.exports = router;
