9007473d3bb99c102a05787eb0b643277163e7fb
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2019-12-08 20:18:55 -0800

    Added all of part1

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,13 +1,18 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 
 const Users = require('../users/users-model');
+// const restricted = require('./restricted-middleware');
 
 router.post('/register', (req, res) => {
     let user = req.body;
 
+    const hash = bcrypt.hashSync(user.password, 12);
+    user.password = hash;
+
     Users.add(user)
         .then(saved => {
-                res.status(201).json(saved);
+            res.status(201).json(saved)
         })
         .catch(error => {
             res.status(500).json(error);
@@ -23,12 +28,12 @@ router.post('/login', (req, res) => {
             if (user) {
                 res.status(200).json({ message: `Welcome ${user.username}!` });
             } else {
-              res.status(401).json({ message: 'Invalid Credentials' });
+                res.status(401).json({ message: 'Invalid Credentials' });
             }
         })
         .catch(error => {
             res.status(500).json(error);
-          });
+        });
 })
 
 module.exports = router;
--- users/users-router.js
+++ users/users-router.js
@@ -1,16 +1,9 @@
 const router = require('express').Router();
 const Users = require('./users-model.js');
-// we bring in the validation middleware and assign the exported method to a
-// constant called "authrequred", so we can use it to protect any handlers that
-// need an authenticated user. Note that we are not doing ANYTHING about
-// authorization in this sample project... If you exist, and can prove you are
-// you by knowing your password, you can do All The Things(tm). 
-// const authrequired = require('../auth/auth-required-middleware.js');
 
-// This method is protected by adding the "authrequired" middleware function to
-// the chain before the actual handler function that finds and returns all the
-// users from the users model (DB). 
-router.get('/', (req, res) => {
+const restricted = require('../auth/restricted-middleware');
+
+router.get('/', restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
